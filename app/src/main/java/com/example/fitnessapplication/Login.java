3592ecbee4699f66743a526eafec0f80838fc069
jannpey <67101174+jannpey@users.noreply.github.com>
2022-09-18 15:45:59 +1200

    Added Code in Login.java and added progress bar in login page

--- app/src/main/java/com/example/fitnessapplication/Login.java
+++ app/src/main/java/com/example/fitnessapplication/Login.java
@@ -3,12 +3,25 @@ package com.example.fitnessapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    EditText mEmail, mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
