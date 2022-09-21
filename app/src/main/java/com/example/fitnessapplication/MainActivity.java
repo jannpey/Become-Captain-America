b7f0bbdb7cb7f270343865f7da1b22aa07c2d7cb
Tong0579 <fpx7578@autuni.ac.nz>
2022-09-21 22:37:37 +1200

    Update Feedback page0

--- app/src/main/java/com/example/fitnessapplication/MainActivity.java
+++ app/src/main/java/com/example/fitnessapplication/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,10 +17,11 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
-    TextView fullName,email;
+    TextView fullName,email,tvFeedback;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
+    RatingBar rbStars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +35,42 @@ public class MainActivity extends AppCompatActivity {
 
         userId = fAuth.getCurrentUser().getUid();
 
+        tvFeedback = findViewById(R.id.tvFeedback);
+        rbStars = findViewById(R.id.rbStars);
+
         DocumentReference documentReference = fStore.collection("users").document(userId);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
                 fullName.setText(documentSnapshot.getString("fName"));
                 email.setText(documentSnapshot.getString("email"));
+            }
+        });
+
+
+        rbStars.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if(rating==0)
+                {
+                    tvFeedback.setText("Very Dissatisfied");
+                }
+                else if(rating==1)
+                {
+                    tvFeedback.setText("Dissatisfied");
+                }
+                else if(rating==2 || rating==3)
+                {
+                    tvFeedback.setText("OK");
+                }
+                else if(rating==4)
+                {
+                    tvFeedback.setText("Satisfied");
+                }
+                else
+                {
+                    tvFeedback.setText("Very Satisfied");
+                }
             }
         });
 
