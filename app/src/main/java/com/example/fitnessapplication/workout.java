123038f07ce94d163b6a9710c0b22165af12a5fc
Issac <xsw7273@autuni.ac.nz>
2022-09-22 23:34:40 +1200

    add workout.java

---
+++ app/src/main/java/com/example/fitnessapplication/workout.java
@@ -0,0 +1,33 @@
+package com.example.fitnessapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class workout extends AppCompatActivity {
+    TextView titlepage, subtitle, title2, subtitle2;
+    View line;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.workout1);
+
+        titlepage=(TextView) findViewById(R.id.titlepage);
+        subtitle=(TextView) findViewById(R.id.subtitle);
+        title2=(TextView) findViewById(R.id.title2);
+        subtitle2=(TextView) findViewById(R.id.subtitle2);
+
+        line = (View) findViewById(R.id.line);
+
+
+
+
+    }
+
+
+}
