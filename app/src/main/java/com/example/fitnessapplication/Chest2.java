58eb927bd05db090f09e461a18bddb77491d9d7a
jannpey <67101174+jannpey@users.noreply.github.com>
2022-10-11 20:43:12 +1300

    Added Workout Page

---
+++ app/src/main/java/com/example/fitnessapplication/Chest2.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chest2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chest2);
+    }
+}
