0ac21d962deb595ef20fb29f4633e95d07b4b899
jannpey <hfw6915@autuni.ac.nz>
2022-10-12 13:28:54 +1300

    Add BMI Test

---
+++ app/src/test/java/com/example/fitnessapplication/BMITest.java
@@ -0,0 +1,25 @@
+package com.example.fitnessapplication;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BMITest {
+
+    BMI1 bmi1;
+
+    @Before
+    public void init() {
+        bmi1 = new BMI1();
+    }
+
+    @Test
+    public void testCalculateBMI() {
+        float expectedValue = 24.38f;
+
+
+
+    }
+
+}
