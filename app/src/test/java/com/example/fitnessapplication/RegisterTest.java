bdd0ec9a61d6936e758e41a86ffb7feca3a3ad67
jannpey <67101174+jannpey@users.noreply.github.com>
2022-10-17 20:21:34 +1300

    Add RegisterTest

---
+++ app/src/test/java/com/example/fitnessapplication/RegisterTest.java
@@ -0,0 +1,13 @@
+package com.example.fitnessapplication;
+
+import junit.framework.TestCase;
+
+public class RegisterTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
